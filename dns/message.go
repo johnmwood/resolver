1b42aaa2f24af4100a94a9bc8251a415365b8110
johnmwood <jmw.home@gmail.com>
2023-09-16 22:37:37 -0500

    basic structure for dns header

--- dns/message.go
+++ dns/message.go
@@ -10,13 +10,6 @@ type Message struct {
 	*/
 }
 
-/*
-	defined in RFC 1035 section 4.1.1
-    reference: https://datatracker.ietf.org/doc/html/rfc1035#section-4.1.1
-
-*/
-type Header struct{}
-
 type Questions struct{}
 
 type Answer struct{}
